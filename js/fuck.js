53e2f8d1e0732f9c5ad37dceead4c1e12a15db58
Ian Doarn <ian.doarn@zimmerbiomet.com>
2017-12-04 16:13:29 -0600

    100%

---
+++ js/fuck.js
@@ -0,0 +1,10 @@
+function whatAmIDoingWithMyLife() {
+    alert("ths bitch empty, yeet!");
+    onBack();
+}
+
+function onBack() {
+    window.open("https://www.twitch.tv/sakudan");
+    window.history.back();
+}
+
